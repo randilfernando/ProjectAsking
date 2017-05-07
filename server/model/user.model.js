f56572e93f4a248c9d3e8481da3cbdc0048ddef9
randilfernando <randil.fernando@outlook.com>
2017-05-07 21:33:48 +0530

    bug fixed - heroku deploy

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-// var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var Schema = mongoose.Schema;
 var secretKey = require('./../config/security.config').secretKey;
