17409ecbc8741790416a027f370de8c0e1d19379
randilfernando <randil.fernando@outlook.com>
2017-05-11 00:28:08 +0530

    Bug fixes

--- server/config/main.config.js
+++ server/config/main.config.js
@@ -7,9 +7,10 @@ var environments = {
   test: "test"
 };
 
-var environment = process.env.NODE_ENV || environments.production; // change environment for the application
+var environment = environments.production; // change environment for the application
 
 module.exports = {
+  host: serverConfig[environment].host,
   port: process.env.PORT || serverConfig[environment].port,
   morganMode: serverConfig[environment].morganMode,
   client: serverConfig[environment].client,
