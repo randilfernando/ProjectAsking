794a1ff2814a4c2c7a386cab2f215ff5d1d9ac89
randilfernando <randil.fernando@outlook.com>
2017-05-23 14:43:36 +0530

    mobile friendly

--- src/app/shared/side-panel/side-panel.component.ts
+++ src/app/shared/side-panel/side-panel.component.ts
@@ -33,8 +33,14 @@ export class SidePanelComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.menuDownHeight = window.innerHeight - (this.menuUp.nativeElement.offsetHeight + 60);
     $(document).ready(function () {
-      $('.dropdown-button').dropdown();
       $('.modal').modal();
+      $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 300
+          edge: 'left', // Choose the horizontal origin
+          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          draggable: true // Choose whether you can drag to open on touch screens
+        }
+      );
     })
   }
 
