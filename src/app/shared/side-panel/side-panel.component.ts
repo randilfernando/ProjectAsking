546a19f8be2b1b940946406255c6b2514d42a39a
randilfernando <randil.fernando@outlook.com>
2017-05-23 16:36:22 +0530

    Fixed sidebar

--- src/app/shared/side-panel/side-panel.component.ts
+++ src/app/shared/side-panel/side-panel.component.ts
@@ -37,7 +37,6 @@ export class SidePanelComponent implements OnInit, AfterViewInit {
       $('.button-collapse').sideNav({
           menuWidth: 300, // Default is 300
           edge: 'left', // Choose the horizontal origin
-          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
           draggable: true // Choose whether you can drag to open on touch screens
         }
       );
