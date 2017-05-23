34b109c73eed40e883a079e31811b15180aaf996
randilfernando <randil.fernando@outlook.com>
2017-05-23 09:06:17 +0530

    Add css into single bundle

--- src/app/shared/text-area/text-area.component.ts
+++ src/app/shared/text-area/text-area.component.ts
@@ -1,10 +1,4 @@
 import {Component,  Output, EventEmitter, AfterViewInit, OnDestroy, Input} from '@angular/core';
-import 'tinymce';
-import 'tinymce/themes/modern';
-
-import 'tinymce/plugins/table';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/paste';
 
 @Component({
   selector: 'ask-text-area',
